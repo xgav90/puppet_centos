e9945ca0fb8e65eb4330ad641643295901791eb9
xgav90 <xandrogavino@outlook.com>
2019-07-15 22:16:10 -0700

    Revise ssh_authorized_key script

--- manifests/ssh_authorized_key.pp
+++ manifests/ssh_authorized_key.pp
@@ -1,5 +1,5 @@
 ssh_authorized_key {'sallymae@contoso.com':
   user => 'root',
   type => 'ssh-rsa',
-  key  =>  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDl85Phsnvao7H8oQwWtkwlSl0t4EYRHvZn3b4almT2q7pcoZ982zbggnN2+mSG+JDughBCoXBHG5Q4xZyP/K0dQDGs9WntrAlbeaDmAbmudcmAj/A+OhXzzcKMVUSZ7RupYWXE46RyvZFoGsjbACPj4lo/tYLSyhN+vyAmMbasCQTxvXlJeAz/st1msZzzpydpjVTFDoAnrXEgBzhpArqY6mC7md3Om0WOCOGcRkMRihCIYRYINty2rJCwHJa2c4fxZckB3w99s32fQElPw1rOR9u7elaSd3p2AZh9KT8SKUVPcTdxTjqdm+1EfEyQmu6wL7Aj4N96yV6QVCgzWGnR sallymae@skyraider.int.hdlx.us',
+  key  =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDl85Phsnvao7H8oQwWtkwlSl0t4EYRHvZn3b4almT2q7pcoZ982zbggnN2+mSG+JDughBCoXBHG5Q4xZyP/K0dQDGs9WntrAlbeaDmAbmudcmAj/A+OhXzzcKMVUSZ7RupYWXE46RyvZFoGsjbACPj4lo/tYLSyhN+vyAmMbasCQTxvXlJeAz/st1msZzzpydpjVTFDoAnrXEgBzhpArqY6mC7md3Om0WOCOGcRkMRihCIYRYINty2rJCwHJa2c4fxZckB3w99s32fQElPw1rOR9u7elaSd3p2AZh9KT8SKUVPcTdxTjqdm+1EfEyQmu6wL7Aj4N96yV6QVCgzWGnR',
 }
