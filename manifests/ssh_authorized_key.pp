4e787e2b1c4c9ef2bce8b17f2931274b83c1ccd4
xgav90 <xandrogavino@outlook.com>
2019-07-15 22:20:02 -0700

    Revise ssh_authorized_key script

--- manifests/ssh_authorized_key.pp
+++ manifests/ssh_authorized_key.pp
@@ -1,4 +1,4 @@
-ssh_authorized_key {'sallymae@contoso.com':
+ssh_authorized_key {'sallymae@skyraider.int.hdlx.us':
   user => 'root',
   type => 'ssh-rsa',
   key  =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDl85Phsnvao7H8oQwWtkwlSl0t4EYRHvZn3b4almT2q7pcoZ982zbggnN2+mSG+JDughBCoXBHG5Q4xZyP/K0dQDGs9WntrAlbeaDmAbmudcmAj/A+OhXzzcKMVUSZ7RupYWXE46RyvZFoGsjbACPj4lo/tYLSyhN+vyAmMbasCQTxvXlJeAz/st1msZzzpydpjVTFDoAnrXEgBzhpArqY6mC7md3Om0WOCOGcRkMRihCIYRYINty2rJCwHJa2c4fxZckB3w99s32fQElPw1rOR9u7elaSd3p2AZh9KT8SKUVPcTdxTjqdm+1EfEyQmu6wL7Aj4N96yV6QVCgzWGnR',
